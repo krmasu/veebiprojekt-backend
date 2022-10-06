2933728e4e3db8882762098260abf99de130ee3a
andrsa <andrsa@ttu.ee>
2022-10-06 22:33:02 +0300

    #3 Created example controller -> service -> repository -> db structure. Added postgres dependency. Connected server with db

---
+++ src/main/java/ee/taltech/iti0302/webproject/repositories/example/Employee.java
@@ -0,0 +1,47 @@
+package ee.taltech.iti0302.webproject.repositories.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    private Integer id;
+    private String firstName;
+    private String idCode;
+    private Integer roleId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getIdCode() {
+        return idCode;
+    }
+
+    public void setIdCode(String idCode) {
+        this.idCode = idCode;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+}
