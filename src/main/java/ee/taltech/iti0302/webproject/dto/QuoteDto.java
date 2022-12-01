98741288270dfb584f02a3793e615b4ddf6bf4f3
Andreas Saarep <andrsa@ttu.ee>
2022-12-01 23:26:19 +0200

    #61 quotedto fix

--- src/main/java/ee/taltech/iti0302/webproject/dto/QuoteDto.java
+++ src/main/java/ee/taltech/iti0302/webproject/dto/QuoteDto.java
@@ -1,10 +1,14 @@
 package ee.taltech.iti0302.webproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuoteDto {
     private String quote;
     private String author;
