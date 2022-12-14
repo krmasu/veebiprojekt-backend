511c259aea827b1bab3604fafc106dc96d71d074
andrsa <andrsa@ttu.ee>
2022-12-14 15:52:49 +0200

    #73 authenticate user service tests

--- src/test/java/ee/taltech/iti0302/webproject/unit/service/AuthenticateUserServiceTest.java
+++ src/test/java/ee/taltech/iti0302/webproject/unit/service/AuthenticateUserServiceTest.java
@@ -1,13 +1,21 @@
 package ee.taltech.iti0302.webproject.unit.service;
 
+import ee.taltech.iti0302.webproject.dto.authentication.LoginRequestDto;
 import ee.taltech.iti0302.webproject.dto.authentication.RegisterUserDto;
+import ee.taltech.iti0302.webproject.dto.project.ProjectDto;
 import ee.taltech.iti0302.webproject.entity.AppUser;
+import ee.taltech.iti0302.webproject.entity.Project;
+import ee.taltech.iti0302.webproject.exception.InvalidCredentialsException;
+import ee.taltech.iti0302.webproject.exception.UserExistsException;
 import ee.taltech.iti0302.webproject.mapper.ProjectMapper;
 import ee.taltech.iti0302.webproject.mapper.ProjectMapperImpl;
 import ee.taltech.iti0302.webproject.mapper.UserMapper;
 import ee.taltech.iti0302.webproject.mapper.UserMapperImpl;
 import ee.taltech.iti0302.webproject.repository.UserRepository;
 import ee.taltech.iti0302.webproject.service.AuthenticateUserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,6 +23,14 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
@@ -36,6 +52,8 @@ class AuthenticateUserServiceTest {
 
     @Spy
     private PasswordEncoder passwordEncoder;
+    private static final long TWELVE_HOURS_AS_MILLI = 43200000;
+
 
     @Test
     void registerUser_ValidData_ReturnsRegisterResponseDto() {
@@ -68,6 +86,116 @@ class AuthenticateUserServiceTest {
 
     @Test
     void registerUser_UsernameExists_ThrowsUserExistsException() {
+        // given
+        RegisterUserDto registerUserDto = RegisterUserDto.builder()
+                .username("username")
+                .email("email")
+                .password("password")
+                .build();
+        given(userRepository.existsByUsername("username")).willReturn(true);
+        // when
+        assertThrows(UserExistsException.class, () -> authenticateUserService.registerUser(registerUserDto));
+        // then
+        then(userRepository).should().existsByUsername("username");
+        then(userRepository).should().existsByEmail("email");
+    }
 
+    @Test
+    void registerUser_EmailExists_ThrowsUserExistsException() {
+        // given
+        RegisterUserDto registerUserDto = RegisterUserDto.builder()
+                .username("username")
+                .email("email")
+                .password("password")
+                .build();
+        given(userRepository.existsByEmail("email")).willReturn(true);
+        // when
+        assertThrows(UserExistsException.class, () -> authenticateUserService.registerUser(registerUserDto));
+        // then
+        then(userRepository).should().existsByUsername("username");
+        then(userRepository).should().existsByEmail("email");
+    }
+
+    @Test
+    void loginUser_GivenValidCredentials_ReturnsLoginResponseDto() {
+        // given
+        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
+                .username("username")
+                .password("password")
+                .build();
+
+        Project project = Project.builder()
+                .id(1)
+                .title("project")
+                .build();
+        List<Project> projects = new ArrayList<>();
+        projects.add(project);
+
+        AppUser user = AppUser.builder()
+                .id(1)
+                .username("username")
+                .password("password")
+                .projects(projects)
+                .build();
+
+        List<ProjectDto> projectDtos = new ArrayList<>();
+        ProjectDto projectDto = ProjectDto.builder()
+                .id(1)
+                .title("project")
+                .build();
+        projectDtos.add(projectDto);
+
+        given(userRepository.findByUsernameIgnoreCase("username")).willReturn(Optional.of(user));
+        given(passwordEncoder.matches("password", "password")).willReturn(true);
+
+        // when
+        var result = authenticateUserService.loginUser(loginRequestDto);
+
+        // then
+        then(userRepository).should().findByUsernameIgnoreCase("username");
+        then(passwordEncoder).should().matches("password", "password");
+        then(projectMapper).should().toDtoList(user.getProjects());
+        then(userMapper).should().toLoginResponseDto(result.getAuthToken(), user.getEmail(), projectDtos, user.getId());
+        assertEquals(projectDtos, result.getProjects());
+    }
+
+    @Test
+    void loginUser_InvalidUsername_ThrowsInvalidCredentialsException() {
+        // given
+        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
+                .username("username")
+                .password("password")
+                .build();
+
+        given(userRepository.findByUsernameIgnoreCase("username")).willReturn(Optional.empty());
+
+        // when
+        assertThrows(InvalidCredentialsException.class, () -> authenticateUserService.loginUser(loginRequestDto));
+
+        // then
+        then(userRepository).should().findByUsernameIgnoreCase("username");
+    }
+
+    @Test
+    void loginUser_InvalidPassword_ThrowsInvalidCredentialsException() {
+        // given
+        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
+                .username("username")
+                .password("password")
+                .build();
+
+        AppUser user = AppUser.builder()
+                .id(1)
+                .username("username")
+                .password("password1")
+                .build();
+
+        given(userRepository.findByUsernameIgnoreCase("username")).willReturn(Optional.of(user));
+        given(passwordEncoder.matches("password", "password1")).willReturn(false);
+        // when
+        assertThrows(InvalidCredentialsException.class, () -> authenticateUserService.loginUser(loginRequestDto));
+        // then
+        then(userRepository).should().findByUsernameIgnoreCase("username");
+        then(passwordEncoder).should().matches("password", "password1");
     }
 }
