4e12256f4a89fd0f524e54a65b916eaeab97d60c
Karl Markus Tarre <katarr@ttu.ee>
2023-01-13 00:04:42 +0200

    Small integration test fix.

--- src/test/java/ee/taltech/iti0302/webproject/WebProjectApplicationTests.java
+++ src/test/java/ee/taltech/iti0302/webproject/WebProjectApplicationTests.java
@@ -1,12 +1,16 @@
 package ee.taltech.iti0302.webproject;
 
+import ee.taltech.iti0302.webproject.integration.AbstractIntegrationTestClass;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class WebProjectApplicationTests {
+@ActiveProfiles("test")
+class WebProjectApplicationTests extends AbstractIntegrationTestClass {
 
     @Test
+    @SuppressWarnings("java:S2699")
     void contextLoads() {
     }
 
